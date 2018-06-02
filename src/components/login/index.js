49da298cfdcc762a2f7540fd2051ba73d136cfd6
zhangzhenfang <fangzhzh@gmail.com>
2018-06-02 15:48:28 +1200

    - react 16.3 (#9)
    - react 16.3 (#9)
    - user context.

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -1,26 +1,30 @@
 import React from 'react';
 import { auth } from 'firebase';
-import firebaseui from 'firebaseui';
+// import firebaseui from 'firebaseui';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+import UserContext from 'userContext';
+import PropTypes from 'prop-types';
 import './login.css';
 
 export default class Login extends React.Component {
+  static propTypes = {
+    setGoogleAccessToken: PropTypes.func,
+  };
+
+  static defaultProps = {
+    setGoogleAccessToken: () => {},
+  };
   constructor(props) {
     super(props);
-    this.ui = new firebaseui.auth.AuthUI(auth());
-    this.setup();
-  }
-  componentDidMount() {}
-
-  componentWillUnmount() {
-    this.ui.reset();
-  }
-
-  setup() {
-    const uiConfig = {
+    this.uiConfig = {
+      signInFlow: 'popup',
       signInSuccessUrl: '/easy',
       callbacks: {
         signInSuccessWithAuthResult(authResult, redirectUrl) {
-          return true;
+          console.log('Login', authResult);
+          this.props.setGoogleAccessToken(authResult.token);
+          
+          return false;
         },
       },
       signInOptions: [
@@ -34,24 +38,57 @@ export default class Login extends React.Component {
             prompt: 'select_account',
           },
         },
-        // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+        // auth.FacebookAuthProvider.PROVIDER_ID,
         // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
         // firebase.auth.GithubAuthProvider.PROVIDER_ID,
         // firebase.auth.EmailAuthProvider.PROVIDER_ID,
         // firebase.auth.PhoneAuthProvider.PROVIDER_ID,
       ],
     };
-
-    // Initialize the FirebaseUI Widget using Firebase.
-    // The start method will wait until the DOM is loaded.
-    this.ui.start('#firebaseui-auth-container', uiConfig);
   }
-
   render() {
     return (
-      <div className="login">
-        <div id="firebaseui-auth-container" />
-      </div>
+      <UserContext.Consumer>
+        { ({ setGoogleAccessToken }) => (
+          <div className="login">
+            <img className="bg" alt="background" />
+            <div className="middle">
+              <StyledFirebaseAuth
+                uiConfig={{
+                  signInFlow: 'popup',
+                  signInSuccessUrl: '/easy',
+                  callbacks: {
+                    signInSuccessWithAuthResult(authResult, redirectUrl) {
+                      console.log('Login signInSuccessWithAuthResult', authResult);
+                      const { accessToken } = authResult.credential;
+                      setGoogleAccessToken(accessToken);
+                      return false;
+                    },
+                  },
+                  signInOptions: [
+                    // Leave the lines as is for the providers you want to offer your users.
+                    {
+                      provider: auth.GoogleAuthProvider.PROVIDER_ID,
+                      scopes: ['https://www.googleapis.com/auth/plus.login'],
+                      customParameters: {
+                        // Forces account selection even when one account
+                        // is available.
+                        prompt: 'select_account',
+                      },
+                    },
+                    // auth.FacebookAuthProvider.PROVIDER_ID,
+                    // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+                    // firebase.auth.GithubAuthProvider.PROVIDER_ID,
+                    // firebase.auth.EmailAuthProvider.PROVIDER_ID,
+                    // firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+                  ],
+                }}
+                firebaseAuth={auth()}
+              />
+            </div>
+          </div>
+        )}
+      </UserContext.Consumer>
     );
   }
 }
--- src/containers/app/index.js
+++ src/containers/app/index.js
@@ -1,20 +1,43 @@
 import React from 'react';
+import UserContext from 'userContext';
 import { Route, Switch } from 'react-router-dom';
 import BusyPage from '../busy/busypage';
 import EasyPage from '../easy/easypage';
 import LoginPage from '../loginPage';
 import About from '../about';
 
-const App = () => (
-  <div>
-    <Switch>
-      <Route path="/login" component={LoginPage} />
-      <Route path="/zhang" component={BusyPage} />
-      <Route path="/chi" component={EasyPage} />
-      <Route path="/about-me" component={About} />
-      <Route path="/" component={EasyPage} />
-    </Switch>
-  </div>
-);
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      googleAccessToken: '',
+      accessToken: '',
+      todos: '',
+      setGoogleAccessToken: (accessToken) => {
+        console.log("App accessToken", accessToken);
+        this.setState({ setGoogleAccessToken: accessToken });
+      },
+      setAccessToken: (accessToken) => {
+        this.setState({
+          accessToken,
+        });
+      },
+    };
+  }
 
-export default App;
+  render() {
+    return (
+      <UserContext.Provider value={this.state}>
+        <div>
+          <Switch>
+            <Route path="/login" component={LoginPage} />
+            <Route path="/zhang" component={BusyPage} />
+            <Route path="/chi" component={EasyPage} />
+            <Route path="/about-me" component={About} />
+            <Route path="/" component={EasyPage} />
+          </Switch>
+        </div>
+      </UserContext.Provider>
+    );
+  }
+}
